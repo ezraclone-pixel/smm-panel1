49acee960da1b341406e82a3fa327df4c9bb0441
ezraclone-pixel <ezraclone@gmail.com>
2026-05-11 15:37:44 +0630

    Update server.js

--- server.js
+++ server.js
@@ -16,12 +16,17 @@ app.get("/", (req, res) => {
 });
 
 // ---------------- MONGODB ----------------
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     console.log("MongoDB Connected");
+
+    // SERVER START should be HERE
+    const PORT = process.env.PORT || 3000;
+
+    app.listen(PORT, () => {
+        console.log(`Server Running On Port ${PORT}`);
+    });
+
 })
 .catch((err) => {
     console.log("MongoDB Error:", err);
